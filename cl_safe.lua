17e3b4d76a1a1d0237409d864e3d60ce8c3f1371
klandestino7 <klandestino6@hotmail.com>
2023-12-21 13:53:16 -0300

    tweak(client): add argument to disable animation

--- cl_safe.lua
+++ cl_safe.lua
@@ -2,7 +2,7 @@ _onSpot = false
 isMinigame = false
 _SafeCrackingStates = "Setup"
 
-function createSafe(combination)
+function createSafe(combination, disableAnimation)
 	local res
 	isMinigame = not isMinigame
 	RequestStreamedTextureDict("MPSafeCracking",false)
@@ -11,7 +11,9 @@ function createSafe(combination)
 	if isMinigame then
 		InitializeSafe(combination)
 		while isMinigame do
-			playFx("mini@safe_cracking","idle_base")
+			if not disableAnimation then
+				playFx("mini@safe_cracking","idle_base")
+			end
 			DrawSprites(true)
 			res = RunMiniGame()
 
